b372966e008b9edcd6dcd7adf7c75f40b33a3031
Your Name <you@example.com>
2024-02-23 14:35:44 +0300

    Added files

---
+++ BookMicroService/src/test/java/ru/gb/JUnitBook.java
@@ -0,0 +1,13 @@
+package ru.gb;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JUnitBook {
+    private Long id;
+    private String title;
+}
